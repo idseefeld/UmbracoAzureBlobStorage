f3d52cc555119656128ba6358cc48281a554ee65
Dirk Seefeld <dirk.seefeld@idseefeld.de>
2015-06-11 16:46:47 +0200

    add virtual path samples

---
+++ InitVirtualPathProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Hosting;
+
+namespace idseefeld.de.UmbracoAzure
+{
+  /// <summary> 
+  ///   Contains the application initialization method 
+  ///   for the sample application. 
+  /// </summary> 
+  public static class AppStart
+  {
+    public static void AppInitialize()
+    {
+      SamplePathProvider sampleProvider = new SamplePathProvider();
+      HostingEnvironment.RegisterVirtualPathProvider(sampleProvider);
+    } 
+  }
+}
